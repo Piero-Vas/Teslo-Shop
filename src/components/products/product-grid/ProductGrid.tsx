8f6f5331837416f074689537ea6df48e1ae8f1e0
Piero-Vas <piero.vasquezriveros@gmail.com>
2023-12-04 23:43:43 -0500

    sidebar design

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -1,24 +1,18 @@
 import { Product } from "@/interfaces";
+import ProductGridItem from "./ProductGridItem";
 
- interface Props {
-    products:Product[];
- }
+interface Props {
+  products: Product[];
+}
 
-export const ProductGrid = ({products}:Props) => {
+export const ProductGrid = ({ products }: Props) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
-        {
-            products.map((product) => {
-                return (
-                     <span key={product.slug}>
-                        {
-                            product.title
-                        }
-                     </span>
-                )
-            })
-        }
-
+      {
+      products.map((product) => {
+        return <ProductGridItem key={product.slug} product={product} />;
+      })
+      }
     </div>
-  )
-} 
+  );
+};
